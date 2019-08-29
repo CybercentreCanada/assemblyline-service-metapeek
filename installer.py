cda3d56d2451adf006bfa82f7617f03c38d878ad
Rushi <rushi.nathvani@165gc.onmicrosoft.com>
2019-08-29 10:16:16 -0400

    Removed installer file

--- installer.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-def install(alsi):
-    alsi.milestone('MetaPeek Complete.')
-
-if __name__ == '__main__':
-    from assemblyline.al.install import SiteInstaller
-    install(SiteInstaller())
